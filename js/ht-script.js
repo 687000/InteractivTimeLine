4f15d4a1872bde0d9788476e1227ab8390d5c350
Jiani Liu <liujianiljn@gmail.com>
2023-02-21 22:48:41 -0800

    feat: finished majority of horizontal time

--- js/ht-script.js
+++ js/ht-script.js
@@ -1,80 +1,305 @@
 const paintings = [
-  {
-    "image": "images/Declaration_of_the_Rights_of_Man_and_of_the_Citizen_in_1789.jpg",
-    "title": "Declaration of the Rights of Man and of the Citizen",
-    "author": "Jean-Jacques-François Le Barbier 1791",
+    {
+    "image": "images/paints/the-tennis-court-oath-15696.jpg",
+    "title": "The Tennis Court Oath",
+    "author": "Jacques-Louis David 1794",
     "intro":"painting 1 intro",
     "content":"event 1 content",
-    "link":"link1",
+    "link":"https://en.wikipedia.org/wiki/The_Tennis_Court_Oath_(David)",
     "eventBlock":1
   },
   {
-    "image": "image.jpg",
-    "title": "painting 2 title",
-    "author": "painting 2 author",
+    "image": "images/paints/Prise_de_la_Bastille.jpg",
+    "title": "The Storming of the Bastille",
+    "author": "Jean-Pierre-Louis-Laurent Houël 1793",
     "intro":"painting 2 intro",
     "content":"event 2 content",
-    "link":"link2",
+    "link":"https://en.wikipedia.org/wiki/File:Prise_de_la_Bastille.jpg",
     "eventBlock":2
-  }
-
+  }, 
+  {
+    "image": "images/paints/Declaration_of_the_Rights_of_Man_and_of_the_Citizen_in_1789.jpg",
+    "title": "Declaration of the Rights of Man and of the Citizen",
+    "author": "Jean-Jacques-François Le Barbier 1791",
+    "intro":"painting 3 intro",
+    "content":"event 3 content",
+    "link":"https://commons.wikimedia.org/wiki/File:Declaration_of_the_Rights_of_Man_and_of_the_Citizen_in_1789.jpg",
+    "eventBlock":3
+  },
+  {
+    "image": "images/paints/Louis_XVI_stopped_at_Varennes.jpeg",
+    "title": "Louis XVI stopped at Varennes",
+    "author": "Mariano Bovi 1796",
+    "intro":"painting 4 intro",
+    "content":"event 4 content",
+    "link":"https://www.nationalgalleries.org/art-and-artists/140793/louis-xvi-stopped-varennes",
+    "eventBlock":4
+  },
+    {
+    "image": "images/paints/Jacques_Bertaux_-_Prise_du_palais_des_Tuileries_-_1793.jpg",
+    "title": "Storming of the Tuileries",
+    "author": "Jean Duplessis-Bertaux 1793",
+    "intro":"painting 5 intro",
+    "content":"event 5 content",
+    "link":"https://commons.wikimedia.org/wiki/File:Jacques_Bertaux_-_Prise_du_palais_des_Tuileries_-_1793.jpg",
+    "eventBlock":5
+  },
+    {
+    "image": "images/paints/Bara_David.jpg",
+    "title": "The Death of Young Bara",
+    "author": "Jacques-Louis David 1794",
+    "intro":"painting 6 intro",
+    "content":"event 6 content",
+    "link":"https://en.wikipedia.org/wiki/The_Death_of_Young_Bara",
+    "eventBlock":6
+  },
+    {
+    "image": "images/paints/Death_of_Marat_by_David.jpg",
+    "title": "The Death of Marat",
+    "author": "Jacques-Louis David 1793",
+    "intro":"painting 7 intro",
+    "content":"event 7 content",
+    "link":"https://en.wikipedia.org/wiki/The_Death_of_Marat",
+    "eventBlock":7
+  },
+    {
+    "image": "images/paints/marie-antionette-execution-533483497.jpg",
+    "title": "Marie Antoinette Being Taken to Her Execution",
+    "author": "William Hamilton 1793",
+    "intro":"painting 8 intro",
+    "content":"event 8 content",
+    "link":"https://www.worldhistory.org/image/16629/marie-antoinette-being-taken-to-her-execution-16-o/",
+    "eventBlock":8
+  },
+    {
+    "image": "images/paints/Portrait_of_a_revolutionary.png",
+    "title": "Portrait of a revolutionary",
+    "author": "Jean-François Sablet 1794",
+    "intro":"painting 9 intro",
+    "content":"event 9 content",
+    "link":"https://artsandculture.google.com/asset/portrait-of-a-revolutionary-fran%C3%A7ois-sablet/ywEpA-sseczDLQ?hl=en",
+    "eventBlock":9
+  },
+    {
+    "image": "images/paints/mots-au-combat-d-pouill-s-de-leurs-uniformes-et-armes-en-1797.jpg!Large.jpg",
+    "title": "Soldiers Killed in Battle Stripped of Their Weapons, Their Uniforms and Their Personal Belongings During the Italian Campaign in 1797",
+    "author": "Nicolas Antoine Taunay 1797",
+    "intro":"painting 10 intro",
+    "content":"event 10 content",
+    "link":"https://www.wikiart.org/en/nicolas-antoine-taunay/soldiers-killed-in-battle-stripped-of-their-weapons-their-uniforms-and-their-personal-belongings-1824",
+    "eventBlock":10
+  },
+    {
+    "image": "images/paints/napoleon_bonaparte_portrait.jpg",
+    "title": "Portrait of Napoléon Bonaparte, The First Council",
+    "author": "Jean Auguste Dominique Ingres 1804",
+    "intro":"painting 11 intro",
+    "content":"event 11 content",
+    "link":"https://www.wikiart.org/en/jean-auguste-dominique-ingres/portrait-of-napol%C3%A9on-bonaparte-the-first-council-1804",
+    "eventBlock":11
+  },
 ]
 const events = [
   {
     "id":1,
-    "year": "1791",
-    "image": "image.jpg",
-    "title": "card1",
-    "time": "event 1 time",
-    "intro":"event 1 intro",
-    "content":"event 1 content",
-    "link":"link1",
+    "year": "1789",
+    "image": "images/events/e01.png",
+    "title": "Tennis Court Oath",
+    "time": "June 20 1789",
+    "content":"The Tennis Court Oath occurred when members of the Third Estate were locked out of the Estates-General meeting by King Louis XVI. They convened a meeting on a nearby indoor tennis court in Versailles and took an oath to continue meeting until they produced a new constitution for France.",
+    "link":"https://en.wikipedia.org/wiki/Tennis_Court_Oath",
     "eventBlock":1
   },
   {
     "id":2,
-    "year": "1792",
-    "image": "image.jpg",
-    "title": "card2",
-    "time": "event 2 time",
-    "intro":"event 2 intro",
-    "content":"event 2 content",
-    "link":"link2",
-    "eventBlock":1
+    "year": "1789",
+    "image": "images/events/e02.png",
+    "title": "Storming of the Bastille",
+    "time": "July 17 1789",
+    "content":"At the time, the Bastille was a symbol of the oppressive monarchy. A large crowd of Parisians stormed the Bastille, hoping to seize weapons and free political prisoners. This marked the first major victory for the revolutionaries.",
+    "link":"https://en.wikipedia.org/wiki/Storming_of_the_Bastille",
+    "eventBlock":2
   },
   {
     "id":3,
-    "year": "1793",
-    "image": "image.jpg",
-    "title": "card3",
-    "time": "event 3 time",
-    "intro":"event 3 intro",
-    "content":"event 3 content",
-    "link":"link3",
-    "eventBlock":2
+    "year": "1789",
+    "image": "images/events/e03.png",
+    "title": "Declaration of the Rights of Man and of the Citizen",
+    "time": "August 27 1789",
+    "content":"The Declaration of the Rights of Man and of the Citizen is a fundamental document drafted by the National Assembly as a response to the abuses of the French monarchy and aristocracy. It aimed to establish the universal principles of individual rights and freedoms. The document is recognized as a milestone in the development of human rights.",
+    "link":"https://en.wikipedia.org/wiki/Declaration_of_the_Rights_of_Man_and_of_the_Citizen",
+    "eventBlock":3
   },
   {
     "id":4,
-    "year": "1794",
-    "image": "image.jpg",
-    "title": "card4",
-    "time": "event 4 time",
-    "intro":"event 4 intro",
-    "content":"event 4 content",
-    "link":"link4",
-    "eventBlock":2
+    "year": "1791",
+    "image": "images/events/e04.png",
+    "title": "Flight to Varennes",
+    "time": "June 20 1791",
+    "content":"King Louis XVI and the royal family attempted to escape Paris and flee to the Austrian Netherlands to eek military aid from foreign powers. Disguised as servants, the royal family was recognized and stopped by authorities in the town of Varennes and brought back to Paris under arrest. The failed escape attempt damaged the king's reputation and further eroded public trust in the monarchy. ",
+    "link":"https://en.wikipedia.org/wiki/Flight_to_Varennes",
+    "eventBlock":4
   },
-  {
+    {
     "id":5,
+    "year": "1792",
+    "image": "images/events/e05.png",
+    "title": "French Revolutionary Wars Began",
+    "time": "April 20 1792",
+    "content":"The wars were sparked by France's territorial expansion and the spread of revolutionary ideals across Europe, beginning in 1792 and lasting until 1802. These wars were fought between France and various European powers. ",
+    "link":"https://en.wikipedia.org/wiki/French_Revolutionary_Wars",
+    "eventBlock":5
+  },
+    {
+    "id":6,
+    "year": "1792",
+    "image": "images/events/e06.png",
+    "title": "Insurrection of 10 August 1792",
+    "time": "August 10 1792",
+    "content":"The Insurrection of 10 August 1792 marked the fall of the monarchy and the rise of the First French Republic. A mob of Parisian revolutionaries stormed the Tuileries Palace, forcing King Louis XVI to flee and leading to the abolition of the monarchy.",
+    "link":"https://en.wikipedia.org/wiki/Insurrection_of_10_August_1792#:~:text=The%20Insurrection%20of%2010%20August,monarchy%20and%20establish%20a%20republic.",
+    "eventBlock":5
+  },
+    {
+    "id":7,
+    "year": "1792",
+    "image": "images/events/e07.png",
+    "title": "French First Republic is Established",
+    "time": "September 22 1792",
+    "content":"The French First Republic was established in 1792, marked by a series of significant changes in French society and politics, including the implementation of democratic principles and the separation of church and state. The French First Republic lasted until 1804, when it was replaced by the First French Empire under Napoleon Bonaparte.",
+    "link":"https://en.wikipedia.org/wiki/French_First_Republic",
+    "eventBlock":5
+  },
+    {
+    "id":8,
+    "year": "1793",
+    "image": "images/events/e08.png",
+    "title": "Execution of Louis XVI",
+    "time": "January 21 1793",
+    "content":"Following the Insurrection of 10 August 1792 and the abolition of the monarchy, Louis XVI was put on trial for high treason by the National Convention. He was found guilty and sentenced to death by guillotine. The execution marked the end of the Bourbon monarchy in France.",
+    "link":"https://en.wikipedia.org/wiki/Execution_of_Louis_XVI",
+    "eventBlock":5
+  },
+    {
+    "id":9,
+    "year": "1793",
+    "image": "images/events/e09.png",
+    "title": "War in the Vendée",
+    "time": "March 07 1793",
+    "content":"The conflict took place in the Vendée region of western France, from 1793 to 1796. The conflict arose from the opposition of the local peasants and nobility to the revolutionary government. The War in the Vendée was one of the bloodiest conflicts of the French Revolution, resulting in the deaths of an estimated 100000 people. ",
+    "link":"https://en.wikipedia.org/wiki/War_in_the_Vend%C3%A9e",
+    "eventBlock":6
+  },
+    {
+    "id":10,
+    "year": "1793",
+    "image": "images/events/e10.png",
+    "title": "Committee of Public Safety is Formed",
+    "time": "March 25 1793",
+    "content":"The Committee of Public Safety was a powerful executive body formed to provide for the defense of the nation against foreign and domestic enemies. Led by Maximilien Robespierre.",
+    "link":"https://en.wikipedia.org/wiki/Committee_of_Public_Safety",
+    "eventBlock":6
+  },
+    {
+    "id":11,
+    "year": "1793",
+    "image": "images/events/e11.png",
+    "title": "Montagnards Overturned Girondins",
+    "time": "May 31 1793",
+    "content":"The Montagnards, a radical political faction advocating for the abolition of the monarchy and the establishment of a republic, overturned the Girondins, a moderate political faction that opposed the radicalism of the Montagnards. The overturn led the dominance of the Montagnards in the National Convention.",
+    "link":"https://en.wikipedia.org/wiki/The_Mountain",
+    "eventBlock":6
+  },
+    {
+    "id":12,
+    "year": "1793",
+    "image": "images/events/e12.png",
+    "title": "Assassination Of Marat",
+    "time": "July 13 1793",
+    "content":"Jean-Paul Marat was a radical politician who supported the Montagnards. He was assassinated by Charlotte Corday, a Girondin sympathizer who believed Marat was contributing to the violence and instability of the revolution. Marat's death heightened tensions between the Montagnards and the Girondins.",
+    "link":"https://en.wikipedia.org/wiki/Jean-Paul_Marat",
+    "eventBlock":7
+  },
+    {
+    "id":13,
+    "year": "1793",
+    "image": "images/events/e13.png",
+    "title": "Robespierre Became The President of Jacobin Club",
+    "time": "August 22 1793",
+    "content":"Maximilien Robespierre was elected to the presidency of the Jacobin Club. Robespierre advocated for the use of terror and violence to achieve the goals of the revolution, and eventually became the leader of the Committee of Public Safety.",
+    "link":"https://en.wikipedia.org/wiki/Maximilien_Robespierre#:~:text=Maximilien%20Fran%C3%A7ois%20Marie%20Isidore%20de,figures%20of%20the%20French%20Revolution.",
+    "eventBlock":7
+  },
+    {
+    "id":14,
+    "year": "1793",
+    "image": "images/events/e14.png",
+    "title": "Reign of Terror Began",
+    "time": "September 17 1793",
+    "content":"The Reign of Terror was a period of extreme violence and political repression, characterized by the establishment of the Committee of Public Safety, which was tasked with eliminating perceived enemies of the revolution and suppressing civil liberties",
+    "link":"https://en.wikipedia.org/wiki/Reign_of_Terror",
+    "eventBlock":7
+  },
+    {
+    "id":15,
+    "year": "1793",
+    "image": "images/events/e15.png",
+    "title": "Marie Antoinette Executed",
+    "time": "October 16 1793",
+    "content":"Marie Antoinette was the queen of France and the wife of King Louis XVI. After the fall of the monarchy, Marie Antoinette was arrested and put on trial for high treason. She was found guilty and sentenced to death by the guillotine.",
+    "link":"https://en.wikipedia.org/wiki/Marie_Antoinette",
+    "eventBlock":8
+  },
+    {
+    "id":16,
+    "year": "1794",
+    "image": "images/events/e16.png",
+    "title": "Thermidorian Reaction",
+    "time": "July 26 1794",
+    "content":"The Thermidorian Reaction was a period of political upheaval, marked by the fall of Maximilien Robespierre and the end of the Reign of Terror. The Thermidorian Reaction was named after the month of Thermidor. The Thermidorian Reaction marked a turning point in the French Revolution.",
+    "link":"https://en.wikipedia.org/wiki/Thermidorian_Reaction",
+    "eventBlock":8
+  },
+    {
+    "id":17,
+    "year": "1794",
+    "image": "images/events/e17.png",
+    "title": "Maximilien Robespierre Executed",
+    "time": " July 27 1794",
+    "content":"Robespierre’s increasing radicalism made him increasingly unpopular even among his own supporters. He was arrested after a failed attempt to give a speech defending himself and were executed on the following day. The execution of Robespierre marked the end of the Reign of Terror.",
+    "link":"https://en.wikipedia.org/wiki/Fall_of_Maximilien_Robespierre#:~:text=The%20Coup%20d'%C3%A9tat%20of,II%20(28%20July%201794).",
+    "eventBlock":9
+  },
+    {
+    "id":18,
     "year": "1795",
-    "image": "image.jpg",
-    "title": "card5",
-    "time": "event 5 time",
-    "intro":"event 5 intro",
-    "content":"event 5 content",
-    "link":"link5",
-    "eventBlock":3
-  },  
+    "image": "images/events/e18.png",
+    "title": "The Directory Replaced The Convention",
+    "time": "November 1795",
+    "content":"The Directory was a five-member executive committee. It governed France from 1795 to 1799, and was established in response to the instability and political upheaval of the previous years.",
+    "link":"https://en.wikipedia.org/wiki/French_Directory",
+    "eventBlock":9
+  },
+    {
+    "id":19,
+    "year": "1796",
+    "image": "images/events/e19.png",
+    "title": "Napoleon Campaign In Italy",
+    "time": "April 2 1796",
+    "content":"Napoleon Bonaparte's campaign in Italy was a series of battles that took place from 1796 to 1797. The campaign helped to establish Bonaparte as a prominent military leader.",
+    "link":"https://en.wikipedia.org/wiki/Army_of_Italy_(France)",
+    "eventBlock":10
+  },
+    {
+    "id":20,
+    "year": "1799",
+    "image": "images/events/e20.png",
+    "title": "French Consulate Formed",
+    "time": "November 10 1799",
+    "content":"After the fall of the Directory, Napoleon seized power and established the French Consulate. The Consulate was a three-man executive committee. The Consulate marked the beginning of the Napoleonic era in France.",
+    "link":"https://en.wikipedia.org/wiki/French_Consulate#:~:text=The%20Consulate%20(French%3A%20Le%20Consulat,this%20period%20of%20French%20history.",
+    "eventBlock":11
+  },
 ]
 events.forEach(event=>{
   addEventCard(event.id,event.year,event.eventBlock,event.image,event.title,event.time);
@@ -85,8 +310,8 @@ function addEventCard(id,year,eventBlock,image, title, time){
 `                <div class="carousel-item" id-value='`+id+`' time-value='`+year+
 `'event-block-value='`+eventBlock+`'  style='transition:0.3s ease-out;'>
                     <img src="`+image+`">
-                    <h3>`+title+`</h3>
-                    <p>`+time+`</p>
+                    <h3>`+time+`</h3>
+                    <p>`+title+`</p>
                 </div>
 `;
 }
@@ -100,7 +325,6 @@ const eventIntroContainer = document.querySelector('.event-intro-container');
 updateEventIntro(events[0].intro,events[0].content,events[0].link);
 function updateEventIntro(intro,content,link){
   eventIntroContainer.innerHTML=`
-                <h3>`+intro+`</h3>
                 <p>`+content+`</p>`;
 }
 window.addEventListener('click',(e)=>{
@@ -202,7 +426,7 @@ paintingContainer.addEventListener('mouseout', () => {
   paintingIntro.style.display="none";
   eventIntroContainer.style.display="grid";
   eventCardsContainer.style.display="block";
-  eventContainer.style.gridTemplateRows = '60% 40%';
+  eventContainer.style.gridTemplateRows = '80% 20%';
 });
 
 const verticalTime = document.getElementById('vertical-time');
@@ -229,4 +453,27 @@ function getCurrentPainting(){
   let currentEventIndex = document.querySelector('.current-card').getAttribute('event-block-value');
   return paintings.findIndex(item => item.eventBlock == currentEventIndex);
 }
+
+const curtain1=document.getElementById("curtain_01");
+console.log(curtain1);
+let updateCoordinate=false;
+window.addEventListener('mousemove', (e) => {
+  let mouseX = e.clientX;
+  let mouseY = e.clientY;
+  if (updateCoordinate) {
+    changeCurtains(mouseX,mouseY)
+    updateCoordinate = false;
+  }
+});
+timer = window.setInterval(function(){
+    updateCoordinate = true;
+}, 500);
+function changeCurtains(X,Y,e){
+  const x = ((window.innerWidth- X)/window.innerWidth/5)+1;
+  const y = (window.innerHeight-Y) /40;
+  curtain1.style.transform = `scale(${x},1) translateY(${y}px)`;  
+}
+
+
+
 
